c1af214f3bb6e4f73a0761886327a896bc23395a
lst <lst@lstui-MacBookAir.local>
2024-05-28 21:02:35 +0900

    falsy값

---
+++ JS/Falsy.js
@@ -0,0 +1,29 @@
+//falsy한 값들 7가지
+let ex1 = undefined;
+let ex2 = null;
+let ex3 = NaN;
+let ex4 = 0;
+let ex5 = -0;
+let ex6 = "";
+let ex7 = 0n; //빅 인티져 라는 특수한 자료형 큰숫자를 저장하는데 사용됨
+
+//if조건문의 조건을 범위를 간결하게 만드는 활용
+function printName(person) {
+  if (!person) {
+    console.log("name값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+
+function printName(person) {
+  // 이런식으로 person의 값을 특정지어 조건을 걸면 나중에 person값이 바뀌면 수정해야되는 단점
+  if (person === undefined) {
+    console.log("name값이 없음");
+    return;
+  }
+  console.log(person.name);
+}
+
+let person = null;
+printName(person);
--- JS/Object.js
+++ JS/Object.js
@@ -1,19 +1,19 @@
 // 1. 객체 생성
-let obj = new Object()//객체 생성자
-let obj2 = {};//객체 리터럴  *훨씬 더 간결해서 이걸로 보통 만듬
+let obj = new Object(); //객체 생성자
+let obj2 = {}; //객체 리터럴  *훨씬 더 간결해서 이걸로 보통 만듬
 
 let person = {
-  name : "이상태",  // 객체의 프로퍼티 (객체 속성) 
-  age : 25,       // key : value , key값으론 문자열이나 숫자 가능, 띄워쓰기 포함하려면 key값에도 따옴표해줘야함
-  hobby : "game",
-  10 : 20,
-  "like cat" : false,
-  man : true,
-  extra1 : function () {}, // 함수가 와도되고
-  extra2 : {}, // 객체가와도되고
-}
+  name: "이상태", // 객체의 프로퍼티 (객체 속성)
+  age: 25, // key : value , key값으론 문자열이나 숫자 가능, 띄워쓰기 포함하려면 key값에도 따옴표해줘야함
+  hobby: "game",
+  10: 20,
+  "like cat": false,
+  man: true,
+  extra1: function () {}, // 함수가 와도되고
+  extra2: {}, // 객체가와도되고
+};
 
-// 객체 프로퍼티를 다루는 법 
+// 객체 프로퍼티를 다루는 법
 // 객체 접근법 1.점표기법   2.괄호표기법
 let name1 = person.name;
 let name2 = person.name2; //이렇게 존재하지않는 프로퍼티에 접근하려고 하면 오류는 안나지만 undefined가 반환됨.
@@ -37,6 +37,21 @@ delete person.job;
 delete person["좋아하는 음식"];
 
 //프로퍼티의 존재 유무를 확인하는 방법 (in 연산자)
-let result1 = "name" in person; 
+let result1 = "name" in person;
 //name이라는 프로퍼티가 person이라는 객체에 있느냐 없느냐를 물어보는것, 리턴값은 Boolean값을 반환함.
 
+//메서드 3가지 형태
+const method1 = {
+  sayHello: function () {
+    console.log("헬로");
+  },
+  sayBye: () => {
+    console.log("잘가");
+  },
+  sayHi() {
+    console.log("안녕하세요"); //메서드 선언
+  },
+};
+
+method1.sayHi();
+method1["sayHi"]();
